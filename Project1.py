6efb2df8ddb6a26de1d74b3f30d081f8b60232fe
Parth23092006 <agravatparth730@gmail.com>
2026-06-12 12:03:58 +0530

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,53 @@
+tasks = []
+
+while True:
+    print("\n========= TO-DO LIST =========")
+    print("1. Add Task")
+    print("2. View Task")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Enter Your Choice (1-4):")
+
+    if choice == "1":
+        task = input("Enter Task:")
+        tasks.append(task)
+        print("✅ Task added successfully!")
+
+    elif choice == "2":
+        if len(tasks)== 0:
+            print("📌 No tasks available.")
+
+        else:
+            print("\n📋 Your Tasks:")
+            for i, task in enumerate(tasks , start=1):
+                print(f"{i}. {task}")
+
+    elif choice == "3":
+        if len(tasks)== 0:
+            print("📌 No tasks available to delete.")
+        
+        else:
+            print("📋 Your Tasks:")
+            for i, task in enumerate(tasks , start=1):
+                print(f"{i}. {task}")
+
+            try:
+                task_num = int(input("Enter task number to delete :"))
+
+                if 1<=task_num<=len(tasks):
+                    removed_task = tasks.pop(task_num - 1)
+                    print(f"❌ Task '{removed_task}' deleted successfully!")
+
+                else:
+                     print("⚠ Invalid task number!")
+
+            except ValueError:
+                print("⚠ Please enter a valid number.")
+
+    elif choice == "4":
+         print("👋 Thank you for using To-Do List!")
+         break
+    
+    else:
+        print("⚠ Invalid choice! Plese select between 1 and 4.")
